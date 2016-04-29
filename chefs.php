e7106ce95a4c935bb31818ab4c6eb2885052d574
kikilin1 <kikiqianlin@gmail.com>
2016-04-29 00:55:26 -0400

    d

---
+++ chefs.php
@@ -0,0 +1,80 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Zentro</title>
+
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="keywords" content="">
+	<meta name="description" content="">
+<!--
+
+Template 2076 Zentro
+
+http://www.tooplate.com/view/2076-zentro
+
+-->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/animate.min.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/nivo-lightbox.css">
+	<link rel="stylesheet" href="css/nivo_themes/default/default.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link href='https://fonts.googleapis.com/css?family=Roboto:400,500' rel='stylesheet' type='text/css'>
+</head>
+<body>
+
+
+
+<!-- navigation section -->
+<section class="navbar navbar-default navbar-fixed-top" role="navigation">
+	<div class="container">
+		<div class="navbar-header">
+			<button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+				<span class="icon icon-bar"></span>
+				<span class="icon icon-bar"></span>
+				<span class="icon icon-bar"></span>
+			</button>
+			<a href="#" class="navbar-brand">ZENTRO</a>
+		</div>
+		<div class="collapse navbar-collapse">
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="index.php">HOME</a></li>
+				<li><a href="foodgallery.php">FOOD GALLERY</a></li>
+				<li><a href="specialmenu.php">SPECIAL MENU</a></li>
+				<li><a href="chefs.php">CHEFS</a></li>
+				<li><a href="contact.php">CONTACT</a></li>
+			</ul>
+		</div>
+	</div>
+</section>
+    <!-- team section -->
+<section id="team" class="parallax-section">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-offset-2 col-md-8 col-sm-12 text-center">
+				<h1 class="heading">Meet Zentro chefs</h1>
+				<hr>
+			</div>
+			<div class="col-md-4 col-sm-4 wow fadeInUp" data-wow-delay="0.3s">
+				<img src="images/team1.jpg" class="img-responsive center-block" alt="team img">
+				<h4>Thanya</h4>
+				<h3>Main Chef</h3>
+			</div>
+			<div class="col-md-4 col-sm-4 wow fadeInUp" data-wow-delay="0.6s">
+				<img src="images/team2.jpg" class="img-responsive center-block" alt="team img">
+				<h4>Lynda</h4>
+				<h3>Pizza Specialist</h3>
+			</div>
+			<div class="col-md-4 col-sm-4 wow fadeInUp" data-wow-delay="0.9s">
+				<img src="images/team3.jpg" class="img-responsive center-block" alt="team img">
+				<h4>Jenny Ko</h4>
+				<h3>New Baker</h3>
+			</div>
+		</div>
+	</div>
+</section>
+    </body>
+</html>
---
+++ foodgallery.php
@@ -0,0 +1,259 @@
+<?php
+// titles
+$titles = array(
+	'Seafood / Shrimp / Lemon',
+	'Lemon-Rosemary Vegetables',
+	'Lemon-Rosemary Salad',
+	'Lemon-Rosemary Pizza',
+	'Lemon-Rosemary Bakery',
+	'Peanut Brittle',
+	'Cappucino',
+	'seafood Plate',
+	'Calzone',
+	'Glazed Doughnut',
+	'Fortune Cookies',
+	'Devils Food Cake',
+	'Peanut Butter Cups',
+	'Crispy Rice Treats',
+	'Cherry Pie',
+	'Apple Turnover',
+	'Napoleon',
+	'Chocolate Chip Cookies',
+	'Chocolate Soufflé',
+	'Walnut Brownies',
+	'Ambrosia Salad',
+	'Peach Cobbler',
+	'Chocolate Eclair',
+	'Chocolate Toaster Tarts',
+	'Candied Ginger',
+	'Spice Cake',
+	'Tapioca Pudding',
+	'Banana Pudding',
+	'Vanilla Cream Pie   ',
+	'Peanut Butter Cookies',
+	'Oatmeal Raisin Cookies',
+	'Chocolate Mousse',
+	'Baklava',
+	'Chocolate Fondue',
+	'Strawberry Shortcake',
+	'Apple Pie a la Mode',
+	'Neapolitan Ice Cream',
+	'Cinnamon Roll',
+	'Gingerbread Cookies',
+	'Gingerbread Dude',
+	'Bread Pudding   ',
+	'Coconut Custard Pie',
+	'Coconut Macaroon',
+	'Ice Cream Cone',
+	'Mint Chocolate Milk Shake',
+	'Pecan Pie',
+	'Smores',
+	'Black Forest Cake',
+	'Fruit Cake',
+	'Pumpkin Nut Bread',
+	'Pumpkin Pie',
+	'Pumpkin Ice Cream',
+	'Rhubarb Crumble',
+	'Carrot Cake',
+	'Ice Cream Cake',
+	'Baked Alaska',
+	'Ice Cream Sandwich',
+	'Hot Fudge Sundae',
+	'Pecan Praline Ice Cream',
+	'Banana Split',
+	'Boston Cream Pie',
+	'Banana Bread',
+	'Chocolate Layer Cake',
+	'Shortbread Cookies',
+	'Oreo Cookies',
+	'Pop Tarts',
+);
+// product details
+$products = array(
+	array('id' => 1, 'title' => 'Seafood / Shrimp / Lemon', 'link' => '95_12345'),
+	array('id' => 2, 'title' => 'Lemon-Rosemary Vegetables', 'link' => '95_123456'),
+	array('id' => 3, 'title' => 'Lemon-Rosemary Salad', 'link' => '95_1234567'),
+	array('id' => 4, 'title' => 'Lemon-Rosemary Pizza', 'link' => '95_12345678'),
+	array('id' => 5, 'title' => 'Lemon-Rosemary Bakery', 'link' => '95_123456789'),
+	array('id' => 6, 'title' => 'Cappucino', 'link' => '90_1'),
+	array('id' => 7, 'title' => 'seafood Plate', 'link' => '90_2'),
+	array('id' => 8, 'title' => 'Calzone', 'link' => '90_3'),
+	array('id' => 9, 'title' => 'Fortune Cookies', 'link' => '167_2835774'),
+	array('id' => 10, 'title' => 'Devils Food Cake', 'link' => '430_3151480'),
+	array('id' => 11, 'title' => 'Peanut Butter Cups', 'link' => '326_2841738'),
+	array('id' => 12, 'title' => 'Crispy Rice Treats', 'link' => '326_2841738'),
+	array('id' => 13, 'title' => 'Cherry Pie', 'link' => '430_3151480'),
+	array('id' => 14, 'title' => 'Apple Turnover', 'link' => '430_3151480'),
+	array('id' => 15, 'title' => 'Napoleon', 'link' => '430_3151480'),
+	array('id' => 16, 'title' => 'Chocolate Chip Cookies', 'link' => '326_2841738'),
+	array('id' => 17, 'title' => 'Chocolate Soufflé', 'link' => '430_3150132'),
+	array('id' => 18, 'title' => 'Walnut Brownies', 'link' => '700_3473780'),
+	array('id' => 19, 'title' => 'Ambrosia Salad', 'link' => '167_2835774'),
+	array('id' => 20, 'title' => 'Peach Cobbler', 'link' => '430_3151480'),
+	array('id' => 21, 'title' => 'Chocolate Eclair', 'link' => '430_3150132'),
+	array('id' => 22, 'title' => 'Chocolate Toaster Tarts', 'link' => '95_2542284'),
+	array('id' => 23, 'title' => 'Candied Ginger', 'link' => '700_3473780'),
+	array('id' => 24, 'title' => 'Spice Cake', 'link' => '430_3151480'),
+	array('id' => 25, 'title' => 'Tapioca Pudding', 'link' => '430_3150132'),
+	array('id' => 26, 'title' => 'Banana Pudding', 'link' => '430_3150132'),
+	array('id' => 27, 'title' => 'Vanilla Cream Pie   ', 'link' => '430_3151480'),
+	array('id' => 28, 'title' => 'Peanut Butter Cookies', 'link' => '95_2542284'),
+	array('id' => 29, 'title' => 'Oatmeal Raisin Cookies', 'link' => '95_2542284'),
+	array('id' => 30, 'title' => 'Chocolate Mousse', 'link' => '430_3150132'),
+	array('id' => 31, 'title' => 'Baklava', 'link' => '700_3473780'),
+	array('id' => 32, 'title' => 'Chocolate Fondue', 'link' => '430_3150132'),
+	array('id' => 33, 'title' => 'Strawberry Shortcake', 'link' => '185_2577502'),
+	array('id' => 34, 'title' => 'Apple Pie a la Mode', 'link' => '167_2835774'),
+	array('id' => 35, 'title' => 'Neapolitan Ice Cream', 'link' => '185_2577502'),
+	array('id' => 36, 'title' => 'Cinnamon Roll', 'link' => '185_2577502'),
+	array('id' => 37, 'title' => 'Gingerbread Cookies', 'link' => '430_3151480'),
+	array('id' => 38, 'title' => 'Gingerbread Dude', 'link' => '430_3151480'),
+	array('id' => 39, 'title' => 'Bread Pudding   ', 'link' => '430_3151480'),
+	array('id' => 40, 'title' => 'Coconut Custard Pie', 'link' => '430_3151480'),
+	array('id' => 41, 'title' => 'Coconut Macaroon', 'link' => '326_2841738'),
+	array('id' => 42, 'title' => 'Ice Cream Cone', 'link' => '700_3473780'),
+	array('id' => 43, 'title' => 'Mint Chocolate Milk Shake', 'link' => '700_3473780'),
+	array('id' => 44, 'title' => 'Pecan Pie', 'link' => '185_2577502'),
+	array('id' => 45, 'title' => 'Smores', 'link' => '167_2835774'),
+	array('id' => 46, 'title' => 'Black Forest Cake', 'link' => '167_2835774'),
+	array('id' => 47, 'title' => 'Fruit Cake', 'link' => '326_2841738'),
+	array('id' => 48, 'title' => 'Pumpkin Nut Bread', 'link' => '430_3150132'),
+	array('id' => 49, 'title' => 'Pumpkin Pie', 'link' => '700_3473780'),
+	array('id' => 50, 'title' => 'Pumpkin Ice Cream', 'link' => '700_3473780'),
+	array('id' => 51, 'title' => 'Rhubarb Crumble', 'link' => '430_3151480'),
+	array('id' => 52, 'title' => 'Carrot Cake', 'link' => '167_2835774'),
+	array('id' => 53, 'title' => 'Ice Cream Cake', 'link' => '700_3473780'),
+	array('id' => 54, 'title' => 'Baked Alaska', 'link' => '430_3150132'),
+	array('id' => 55, 'title' => 'Ice Cream Sandwich', 'link' => '326_2841738'),
+	array('id' => 56, 'title' => 'Hot Fudge Sundae', 'link' => '430_3151480'),
+	array('id' => 57, 'title' => 'Pecan Praline Ice Cream', 'link' => '95_2542284'),
+	array('id' => 58, 'title' => 'Banana Split', 'link' => '167_2835774'),
+	array('id' => 59, 'title' => 'Boston Cream Pie', 'link' => '430_3151480'),
+	array('id' => 60, 'title' => 'Banana Bread', 'link' => '430_3151480'),
+	array('id' => 61, 'title' => 'Chocolate Layer Cake', 'link' => '326_2841738'),
+	array('id' => 62, 'title' => 'Shortbread Cookies', 'link' => '185_2577502'),
+	array('id' => 63, 'title' => 'Oreo Cookies', 'link' => '326_2841738'),
+	array('id' => 64, 'title' => 'Pop Tarts', 'link' => '430_3151480'),
+);
+$maxProducts = count($products);
+// use the "ternary" operator to check if page param is set
+$page = (isset($_GET['page'])) ? (int) $_GET['page'] : 0;
+// use "ternary" operator to check to see if page is 0
+$prev = ($page == 0) ? 0 : $page - 1;
+$next = $page + 1;
+$linesPerPage = 8;
+
+function displayProducts($page, $linesPerPage, $maxProducts, $products)
+{
+	$offset = $page * $linesPerPage;
+	$output = '';
+	for($x = 0; $x < $linesPerPage; $x++) {
+		if ($x + $offset >= $maxProducts) {
+			break;
+		}
+		$output .= '<li>';
+		$output .= '<div class="image col-md-3">';
+		$output .= '<a href="detail.html">';
+		$output .= '<img src="images/'
+				 . $products[$x + $offset]['link']
+				 . '.scale_20.JPG" alt="'
+				 . $products[$x + $offset]['title']
+				 . '" width="190" height="130"/>';
+		$output .= '</a>';
+		
+		$output .= '<div class="detail">';
+		$output .= '<p class="name"><a href="detail.html">'
+				 . $products[$x + $offset]['title']
+				 . '</a></p>';
+		$output .= '<p class="view"><a href="specialmenu.php">price</a> | <a href="specialmenu.php">view menu >></a></p>';
+		$output .= '</div>';
+        $output .= '</div>';
+		$output .= '</li>';
+	}
+	return $output;
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Zentro</title>
+
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="keywords" content="">
+	<meta name="description" content="">
+<!--
+
+Template 2076 Zentro
+
+http://www.tooplate.com/view/2076-zentro
+
+-->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/animate.min.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/nivo-lightbox.css">
+	<link rel="stylesheet" href="css/nivo_themes/default/default.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link href='https://fonts.googleapis.com/css?family=Roboto:400,500' rel='stylesheet' type='text/css'>
+</head>
+<body>
+
+
+
+<!-- navigation section -->
+<section class="navbar navbar-default navbar-fixed-top" role="navigation">
+	<div class="container">
+		<div class="navbar-header">
+			<button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+				<span class="icon icon-bar"></span>
+				<span class="icon icon-bar"></span>
+				<span class="icon icon-bar"></span>
+			</button>
+			<a href="#" class="navbar-brand">ZENTRO</a>
+		</div>
+		<div class="collapse navbar-collapse">
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="index.php">HOME</a></li>
+				<li><a href="foodgallery.php">FOOD GALLERY</a></li>
+				<li><a href="specialmenu.php">SPECIAL MENU</a></li>
+				<li><a href="chefs.php">CHEFS</a></li>
+				<li><a href="contact.php">CONTACT</a></li>
+			</ul>
+		</div>
+	</div>
+</section>
+    
+    <!-- gallery section -->
+<section id="gallery" class="parallax-section">
+	<div class="container">
+		<div class="row">
+			<div class=" col-sm-12 text-center">
+				<h1 class="heading">Food Gallery</h1>
+				<hr>
+			</div>
+            <div class="product-list">
+                <div>
+                    <div class="text-center">
+                            <a class="pages" href="foodgallery.php?page=<?php echo $prev; ?>">&lt;prev</a>
+                    &nbsp;|&nbsp;
+                            <a class="pages" href="foodgallery.php?page=<?php echo $next; ?>">next&gt;</a>
+                    </div>
+                    <div class="col-xs-12">
+                        
+                        <ul>
+                            <?php echo displayProducts($page, $linesPerPage, $maxProducts, $products); ?>
+                        </ul>
+                       
+                    </div>
+                </div>
+	</div>
+			
+			</div>
+		</div>
+	</div>
+</section>
+    </body>
+</html>
